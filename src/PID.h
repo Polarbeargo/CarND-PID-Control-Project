66da08eb12c1036f70acfa88616bf1f8f2ff4741
ali <bow1226@gmail.com>
2017-12-16 09:06:48 +0800

    Format

--- src/PID.h
+++ src/PID.h
@@ -1,7 +1,8 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
+class PID
+{
 public:
   /*
   * Errors
@@ -12,7 +13,7 @@ public:
 
   /*
   * Coefficients
-  */ 
+  */
   double Kp;
   double Ki;
   double Kd;
