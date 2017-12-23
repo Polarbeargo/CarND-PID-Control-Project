95d046977cb05e8d772439f8323e5a6c378021f2
ali <bow1226@gmail.com>
2017-12-23 17:11:12 +0800

    Initialize the pid variable & Complete the PID class.
    Initialize the pid variable.
    Complete the PID class.

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,12 +12,22 @@ PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd)
 {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    p_error = d_error = i_error = 0.0;
 }
 
 void PID::UpdateError(double cte)
 {
+    double pre_cte = cte;
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - pre_cte;
 }
 
 double PID::TotalError()
 {
+    -Kp *p_error * -Ki *i_error - Kd *d_error;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.15, 0.0, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -62,6 +63,7 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
 
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
